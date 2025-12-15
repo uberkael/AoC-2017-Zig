14bd00b88e8e5e010fd3302bbad99ecfbb0e17dd
KaeL <uberkael@gmail.com>
2025-12-15 05:52:17 +0100

    Utils add new functions

--- src/utils.zig
+++ src/utils.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 
+/// Reads the entire content of a file into a byte slice
 pub fn readFile(allocator: std.mem.Allocator, path: []const u8) []u8 {
     var file = std.fs.cwd().openFile(path, .{}) catch {
         std.debug.panic("file not found: {s}\n", .{path});
@@ -10,11 +11,49 @@ pub fn readFile(allocator: std.mem.Allocator, path: []const u8) []u8 {
     };
 }
 
-pub fn linesIterator(data: []u8) std.mem.TokenIterator(u8, .scalar) {
+/// Returns an iterator that yields lines from the given data
+pub fn linesIterator(data: []const u8) std.mem.TokenIterator(u8, .scalar) {
     return std.mem.tokenizeScalar(u8, data, '\n');
 }
 
-pub fn cleanInput(input: []u8) []const u8 {
+/// Returns an ArrayList containing all the lines as byte slices
+pub fn getLinesList(
+    allocator: std.mem.Allocator,
+    data: []const u8,
+) std.ArrayListUnmanaged([]const u8) {
+    var lines = std.ArrayListUnmanaged([]const u8){};
+    lines.ensureTotalCapacity(allocator, 16) catch |err| {
+        std.debug.panic("Error allocating capacity: {}", .{err});
+    };
+    var iter = linesIterator(data);
+    while (iter.next()) |line| {
+        lines.append(allocator, line) catch |err| {
+            std.debug.panic("Error appending line: {}", .{err});
+        };
+    }
+    return lines;
+}
+
+/// Returns a slice of slices containing all the lines
+pub fn getLinesSlice(
+    allocator: std.mem.Allocator,
+    data: []const u8,
+) [][]const u8 {
+    var lines = std.ArrayListUnmanaged([]const u8){};
+    defer lines.deinit(allocator);
+    var iter = linesIterator(data);
+    while (iter.next()) |line| {
+        lines.append(allocator, line) catch |err| {
+            std.debug.panic("Error appending line: {}", .{err});
+        };
+    }
+    return lines.toOwnedSlice(allocator) catch |err| {
+        std.debug.panic("Error converting to owned slice: {}", .{err});
+    };
+}
+
+/// Removes trailing newlines from the input
+pub fn cleanInput(input: []const u8) []const u8 {
     var lines = linesIterator(input);
     return lines.next() orelse "";
 }
